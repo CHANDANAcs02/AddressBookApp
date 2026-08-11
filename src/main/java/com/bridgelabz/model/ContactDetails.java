356c2f310b5625a99f04bad5b2fe4a856df51c65
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-08-11 14:10:06 +0530

    [Chandana]:"Add contact details model"

---
+++ src/main/java/com/bridgelabz/model/ContactDetails.java
@@ -0,0 +1,65 @@
+package com.bridgelabz.model;
+
+import java.util.List;
+
+public class ContactDetails {
+
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+    private List<Address> addresses;
+
+    public ContactDetails() {
+    }
+
+    public ContactDetails(int id, String name, String phone,
+                          String email, List<Address> addresses) {
+
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.addresses = addresses;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+}
